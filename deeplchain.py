c23c17fa0aa5699f2edccbcd93f2c69f2518516f
Manazhy <157498167+Manazhy@users.noreply.github.com>
2024-10-20 08:52:36 +0530

    Add files via upload

---
+++ deeplchain.py
@@ -0,0 +1,90 @@
+import os
+import json
+import asyncio
+from datetime import datetime
+from colorama import *
+
+mrh = Fore.LIGHTRED_EX
+pth = Fore.LIGHTWHITE_EX
+hju = Fore.LIGHTGREEN_EX
+kng = Fore.LIGHTYELLOW_EX
+bru = Fore.LIGHTBLUE_EX
+reset = Style.RESET_ALL
+htm = Fore.LIGHTBLACK_EX
+
+last_log_message = None 
+
+
+def banner():
+    banner = r"""
+   _____ __________  ________  ______   ______  __   
+  / ___// ____/ __ \/  _/ __ \/_  __/  / __ ) \/ /   
+  \__ \/ /   / /_/ // // /_/ / / /    / __  |\  /    
+ ___/ / /___/ _, _// // ____/ / /    / /_/ / / /     
+/____/\____/_/ |_/___/_/     /_/    /_____/ /_/      
+                                                     """ 
+    print(Fore.GREEN + Style.BRIGHT + banner + Style.RESET_ALL)  
+    
+    
+    banner = r"""
+   
+███████╗██╗  ██╗ █████╗ ██████╗  ██████╗ ██╗    ██╗
+██╔════╝██║  ██║██╔══██╗██╔══██╗██╔═══██╗██║    ██║
+███████╗███████║███████║██║  ██║██║   ██║██║ █╗ ██║
+╚════██║██╔══██║██╔══██║██║  ██║██║   ██║██║███╗██║
+███████║██║  ██║██║  ██║██████╔╝╚██████╔╝╚███╔███╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚══╝╚══╝ 
+                                                     """ 
+    print(Fore.RED + Style.BRIGHT + banner + Style.RESET_ALL)  
+    print(hju + f"𝗦𝗖𝗥𝗜𝗣𝗧 𝗕𝗬 𝗦𝗛𝗔𝗗𝗢𝗪")
+    print(mrh + f" FREE TO USE = CRYPTO CRACKERS")
+    print(mrh + f" Admins '{hju}APK SHADOW {mrh}' Criminal")
+    log_line()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def read_config():
+    config_path = os.path.join(os.path.dirname(__file__), '../config.json')
+    with open(config_path, 'r') as file:
+        try:
+            config_content = file.read()
+            return json.loads(config_content)
+        except json.JSONDecodeError as e:
+            return {}
+        
+def log(message, **kwargs):
+    global last_log_message
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    flush = kwargs.pop('flush', False)
+    end = kwargs.pop('end', '\n')
+    if message != last_log_message:
+        print(f"{htm}[{current_time}] {message}", flush=flush, end=end)
+        last_log_message = message
+
+def log_error(message):
+    with open('last.log', 'a') as log_file:
+        log_file.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - ERROR - {message}\n")
+
+def log_line():
+    print(pth + "~" * 60)
+
+def awak():
+    clear()
+    banner()
+    log_line()
+
+async def countdown_timer(seconds):
+    while seconds:
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        h = str(h).zfill(2)
+        m = str(m).zfill(2)
+        s = str(s).zfill(2)
+        print(f"{pth}please wait until {h}:{m}:{s} ", flush=True, end="\r")
+        seconds -= 1
+        await asyncio.sleep(1)
+    print(f"{pth}please wait until {h}:{m}:{s} ", flush=True, end="\r")
+
+def number(number):
+    return "{:,.0f}".format(number)
